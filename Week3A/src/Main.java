f6e519bb95c397cf81e0f09a558582759071fbc6
George <gs.martin@ulster.ac.uk>
2026-02-08 10:03:04 +0000

    //Week3A Solutions

---
+++ Week3A/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arrayRandom = new int[100];
+        for(int i = 0; i<arrayRandom.length;i++) {
+            arrayRandom[i] = random.nextInt(100) + 1;
+        }
+
+        System.out.println(Arrays.toString(arrayRandom));
+
+        arrayRandom = SortComparisons.insertionSort(arrayRandom);
+        System.out.println(Arrays.toString(arrayRandom));
+
+
+    }
+}
